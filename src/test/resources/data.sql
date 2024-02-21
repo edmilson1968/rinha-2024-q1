4c54823e88d15fa653b0b416d7c277fdd151fbc6
Edmilson Figueiredo <edmilson.figueiredo@gmail.com>
2024-02-20 22:20:40 -0300

    arranquei os testes

--- src/test/resources/data.sql
+++
@@ -1,5 +0,0 @@
-insert into clientes (id, limite, saldo) values (1, 1000 * 100, 0);
-insert into clientes (id, limite, saldo) values (2, 800 * 100, 0);
-insert into clientes (id, limite, saldo) values (3, 10000 * 100, 0);
-insert into clientes (id, limite, saldo) values (4, 100000 * 100, 0);
-insert into clientes (id, limite, saldo) values (5, 5000 * 100, 0);
